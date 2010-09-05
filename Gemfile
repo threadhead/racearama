b7d6fef8ad073b05fe7febedeb9e96ffd6c8a9f8
Karl Smith <threadhead@gmail.com>
2010-09-04 20:37:45 -0700

    add devise and user model

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.0.0'
 
 gem 'mysql2'
 gem 'compass'
+gem 'devise'
 
 # Use unicorn as the web server
 # gem 'unicorn'
---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  # defaults removed -> :registerable :recoverable :validatable
+  devise :database_authenticatable, :rememberable, :trackable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+end
