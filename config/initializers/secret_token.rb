6598baad52a4ae1629ee097bd3c14324573fa81b
Karl Smith <threadhead@gmail.com>
2010-09-04 18:51:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Racearama::Application.config.secret_token = '51c474634f423d1da785a216e27280d52cef0d47aba5d0aa30a676e34555b35f195bc664d1136eb4c323702756bdf9fea3f4fc641b04f267d247dbaf244198b5'
