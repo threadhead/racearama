694ecc40fd92935c9969cbdf18efd79012a6c296
Karl Smith <threadhead@gmail.com>
2010-09-27 20:56:29 -0700

    working on track manager/index, events views

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,7 @@
 class EventsController < ApplicationController
+  before_filter :authenticate_user!
+  authorize_resource
+  
   def index
     @events = Event.all
   end
--- app/controllers/track_manager_controller.rb
+++ app/controllers/track_manager_controller.rb
@@ -1,5 +1,14 @@
 class TrackManagerController < ApplicationController
+  before_filter :store_params_and_authenticate
+    
   def index
+    session[:username] = nil
+  end
+
+  private
+  def store_params_and_authenticate
+    session[:username] = "track_manager"
+    authenticate_user!
   end
 
 end
